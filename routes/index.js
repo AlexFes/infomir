52ab33468243465ca77ffc3a037acd4743daeb93
AlexFes <alexeiruxur@gmail.com>
2017-12-17 23:43:07 +0300

    Made some nice responsive styles.

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,26 @@ let express = require('express');
 let Product = require('../models/product');
 let mongoose = require('mongoose');
 let Cart = require('../models/cart');
+//let generateArrays = require('../models/cart');
 let router = express.Router();
 
 mongoose.Promise = global.Promise;
 
 router.get('/', (req, res, next) => {
     Product.find((err, products) => {
-        res.locals.products = products;
+        let cart = new Cart(req.session.cart ? req.session.cart : {});
 
-        res.render('shop/index', { title: 'Infomir' });
+        if (!cart) {
+            res.render('shop/index', {title: 'Infomir', productsArray: null, products: products});
+        }
+
+        res.render('shop/index', {title: 'Infomir', productsArray: cart.generateArrays(), products: products});
     });
 });
 
 router.post('/add-to-cart/:id', (req, res, next) => {
     let productId = req.params.id;
     let productQty = req.body.productQty;
-
-    console.log(productQty);
 
     let cart = new Cart(req.session.cart ? req.session.cart : {});
 
@@ -29,21 +32,10 @@ router.post('/add-to-cart/:id', (req, res, next) => {
             req.session.cart = cart;
             console.log(req.session.cart);
 
-            res.redirect('/');
+            // res.locals.productsArray = cart.generateArrays();
+
+            res.redirect('/#shop');
         });
 });
 
 module.exports = router;
-
-    // Product.findById(productId, (err, product) => {
-    //     if (err) {
-    //        return res.redirect('/');
-    //     }
-    //
-    //     cart.add(product, product._id);
-    //
-    //     req.session.cart = cart;
-    //     console.log(req.session.cart);
-    //
-    //     res.redirect('/');
-    // });
